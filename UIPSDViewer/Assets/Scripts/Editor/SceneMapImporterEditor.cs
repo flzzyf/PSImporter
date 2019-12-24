d1221336bdcc978b6376fd10d19663a11ae310b8
Flzzyf <2981019122@qq.com>
2019-12-24 17:35:02 +0800

    1

---
+++ UIPSDViewer/Assets/Scripts/Editor/SceneMapImporterEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneMapImporter))]
+public class SceneMapImporterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SceneMapImporter main = target as SceneMapImporter;
+
+        if (GUILayout.Button("生成"))
+        {
+            main.GenerateLayers();
+        }
+    }
+}
---
+++ UIPSDViewer/Assets/Scripts/SceneMapImporter2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+
+[ScriptedImporter(1, "psb")]
+public class SceneMapImporter2 : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        //name = GetFileName(ctx.assetPath);
+        Debug.Log("qe"); 
+
+        //var t = AssetDatabase.LoadAssetAtPath(ctx.assetPath, typeof(Texture2D));
+        //Debug.Log(t.name);
+
+        //ctx.AddObjectToAsset("main obj", cube);
+        //ctx.SetMainObject(cube);
+
+        //var material = new Material(Shader.Find("Standard"));
+        //material.name = "mat";
+        //ctx.AddObjectToAsset("mat", material);
+    }
+}
